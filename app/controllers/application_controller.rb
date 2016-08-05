23ceb460b29241e03f3a475f18d8bbd57822b6de
Cameron Perrin <perrin.cameron@gmail.com>
2016-08-05 16:54:24 +0100

    photos can now be added to the user and to the items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
 
   protected
      def configure_permitted_parameters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :full_name, :password, :password_confirmation])
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :full_name, :user_image, :password, :password_confirmation])
 
-       devise_parameter_sanitizer.permit(:account_update, keys: [:email, :username, :full_name, :password, :password_confirmation])
+       devise_parameter_sanitizer.permit(:account_update, keys: [:email, :username, :full_name, :user_image, :password, :password_confirmation])
      end
 
      def after_sign_in_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :items
+  mount_uploader :user_image, UserImageUploader
 end

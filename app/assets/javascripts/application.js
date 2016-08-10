d39fa940deb98c1e656dbc2911f8635a6b054223
Cameron Perrin <perrin.cameron@gmail.com>
2016-08-10 09:00:27 +0100

    Fixed the issue where javascript was not loading properly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,104 +14,101 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-document.addEventListener( 'DOMContentLoaded', function () {
-
-console.log("JS loaded");
-
-function shake(div){
-    var interval = 100;
-    var distance = 10;
-    var times = 4;
-    $(div).css('position','relative');
-    for(var iter=0;iter<(times+1);iter++){                            
-        $(div).animate({ 
-            left:((iter%2==0 ? distance : distance*-1))
-            },interval);                                   
-    }                                           
-    $(div).animate({ left: 0},interval);                            
-}
-
-function promoChangeWord(){
-  if(photoNum === 1) {
-    $(".changing-promo").html('Fast');
-  } else if(photoNum === 2) {
-    $(".changing-promo").html('Reliable');
-  } else if(photoNum === 3) {
-    $(".changing-promo").html('Friendly');
-  };
-};
-
-var photoNum = 3
-
-$(".promo").css("background-image", "url(assets/promo-photo"+photoNum+".png)");
-promoChangeWord()
+$(document).on('turbolinks:load', function() {
 
 
+    console.log("JS loaded");
 
-
-$(".forward").on("click", function(){
-  if(photoNum > 2){
-    shake($(".promo"));
-  } else {
-  photoNum++
-  $(".promo").css("background-image", "url(assets/promo-photo"+photoNum+".png)");
-  $(".changing-promo").fadeOut('slow');
-  setTimeout(function(){
-    promoChangeWord()
-  }, 600);
-  $(".changing-promo").fadeIn('slow');
-  }
-});
-
-
-
-$(".backward").on("click", function(){
-  if(photoNum < 2){
-    shake($(".promo"));
-  } else {
-    photoNum--
-    $(".promo").css("background-image", "url(assets/promo-photo"+photoNum+".png)");
-    $(".changing-promo").fadeOut('slow');
-    setTimeout(function(){
-      promoChangeWord()
-    }, 600);
-    $(".changing-promo").fadeIn('slow');
-  }
-});
-
-
-
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-  $( ".menu" ).slideToggle( "fast", function() {
-  });
-});
-
-var $items = $(".items")
-
-$(".search-items").on('keyup', function(){
-  var input = this;
-  $items.each(function() {
-    var userInput = $(this).text().toLowerCase()
-    var itemTitle = $(input).val().toLowerCase()
-
-
-    if(userInput.match(itemTitle)){
-      $(this).show()
-    } else {
-      $(this).hide()
+    function shake(div){
+        var interval = 100;
+        var distance = 10;
+        var times = 4;
+        $(div).css('position','relative');
+        for(var iter=0;iter<(times+1);iter++){                            
+            $(div).animate({ 
+                left:((iter%2==0 ? distance : distance*-1))
+                },interval);                                   
+        }                                           
+        $(div).animate({ left: 0},interval);                            
     }
-  })
-})
+
+    function promoChangeWord(){
+      if(photoNum === 1) {
+        $(".main-changing-promo").html('Our shipping times are');
+        $(".changing-promo").html('Fast');
+      } else if(photoNum === 2) {
+        $(".main-changing-promo").html('Our delivery men are');
+        $(".changing-promo").html('Reliable');
+      } else if(photoNum === 3) {
+        $(".main-changing-promo").html('Our customer service is');
+        $(".changing-promo").html('Friendly');
+      };
+    };
+
+    var photoNum = 3
+
+    $(".promo").css("background-image", "url(assets/promo-photo"+photoNum+".png)");
+    promoChangeWord()
 
 
 
 
+    $(".forward").on("click", function(){
+      if(photoNum > 2){
+        shake($(".promo"));
+      } else {
+      photoNum++
+      $(".promo").css("background-image", "url(assets/promo-photo"+photoNum+".png)");
+      $(".changing-promo").fadeOut('slow');
+      $(".main-changing-promo").fadeOut('slow');
+      setTimeout(function(){
+        promoChangeWord()
+      }, 600);
+      $(".main-changing-promo").fadeIn('slow');
+      $(".changing-promo").fadeIn('slow');
+      }
+    });
 
 
 
+    $(".backward").on("click", function(){
+      if(photoNum < 2){
+        shake($(".promo"));
+      } else {
+        photoNum--
+        $(".promo").css("background-image", "url(assets/promo-photo"+photoNum+".png)");
+        $(".main-changing-promo").fadeOut('slow');
+        $(".changing-promo").fadeOut('slow');
+        setTimeout(function(){
+          promoChangeWord()
+        }, 600);
+        $(".main-changing-promo").fadeIn('slow');
+        $(".changing-promo").fadeIn('slow');
+      }
+    });
 
 
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+      $( ".menu" ).slideToggle( "fast", function() {
+      });
+    });
+
+    var $items = $(".items")
+
+    $(".search-items").on('keyup', function(){
+      var input = this;
+      $items.each(function() {
+        var userInput = $(this).text().toLowerCase()
+        var itemTitle = $(input).val().toLowerCase()
 
 
-});
+        if(userInput.match(itemTitle)){
+          $(this).show()
+        } else {
+          $(this).hide()
+        }
+      })
+    })
+      
+  });

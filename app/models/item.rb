ac3158e882eb21484b85fcac81cbe1c7a304e24e
Cameron Perrin <perrin.cameron@gmail.com>
2016-08-11 19:56:11 +0100

    fixed issues where it would give errors if the item had no image and added price totalling for the cart.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :user
-  has_many :wishlists
-  has_many :carts
+  has_many :wishlists, dependent: :destroy
+  has_many :carts, dependent: :destroy
   mount_uploader :item_image, ItemImageUploader
 end
